9188289bc181971c09462362fd0979bdbf2211e4
udokamma <ezuvictor@gmail.com>
2024-08-18 19:50:43 +0000

    ppdi

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,26 @@
+# 0-strace_is_your_friend.pp
+
+# Example: Fixing a file permission issue on a specific directory
+file { '/var/www/html/some-directory':
+  ensure  => 'directory',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0755',
+  recurse => true,
+}
+
+# Example: Ensure a specific configuration file is present
+file { '/etc/apache2/sites-available/000-default.conf':
+  ensure  => 'file',
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
+  source  => 'puppet:///modules/apache/000-default.conf',
+  notify  => Exec['apache_reload'],
+}
+
+# Reload Apache after the fix
+exec { 'apache_reload':
+  command     => '/usr/sbin/service apache2 reload',
+  refreshonly => true,
+}
---
+++ 0x17-web_stack_debugging_3/README.md
@@ -0,0 +1,1 @@
+This project is all about web stack debugging in software production.
